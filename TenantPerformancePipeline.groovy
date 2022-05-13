e45faaa12bad8b0ca19125f12bb2fb3513aff228
Darya Shestakova <80690876+DaryaShestakova@users.noreply.github.com>
2022-05-13 10:56:09 +0300

    initial commit

--- TenantPerformancePipeline.groovy
+++ TenantPerformancePipeline.groovy
@@ -1,17 +1,13 @@
 
 // Param from JENKINS
-appBuildNumber = "${env.BUILD_NUMBER}"
 users = "${users}"
 during = "${during}"
 
 // Vars for Job Status
-buildStatus = "BUILD_STARTED"
 successfulStatus = "SUCCESS"
 failedStatus = "FAILURE"
 abortedStatus = "ABORTED"
 jobStatus = failedStatus // By default
-
-def reportLink = "http://idtq-automation.awsqa.idt.net/xxx/${appBuildNumber}"
 
 // Commands
 buildCommandToExecute = "mvn -B clean package"
@@ -19,19 +15,12 @@ testCommandToExecute = "mvn clean gatling:test -Dusers=${users} -Dduring=${durin
 
 // Start testing
 node("master") {
-    mailToRecipients(buildStatus, reportLink, false)
 
     try {
         stage('Run the tests') {
-            withEnv(["JAVA_HOME=${tool 'jdk8'}", "PATH+MAVEN=${tool 'mvn3.2.5'}/bin:${env.JAVA_HOME}/bin"]) {
-
-                git branch: 'develop', credentialsId: 'IDTQE-JENKINS', url: 'https://github.com/coretech/n2p-tenant-gatling-performance.git'
-                // sh "java -version"
-                // sh "mvn -v"
                 sh "echo 'STARTING...'"
                 sh "${buildCommandToExecute}"
                 sh "${testCommandToExecute}"
-            }
             jobStatus = successfulStatus
         }
     } catch (InterruptedException e) {
